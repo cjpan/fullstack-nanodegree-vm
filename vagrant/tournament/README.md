929cbb01274252bbcc0b5e980470d50bbcaf3da5
Cheng-Jie Pan <chengjie.pan@gmail.com>
2015-11-10 22:21:15 +0800

    P2 Tournament: Modify according to review comments
    1. modify winner_id and loser_id to integer;
    2. update the readme.md file.

--- vagrant/tournament/README.md
+++ vagrant/tournament/README.md
@@ -6,7 +6,8 @@
 1. Start Vagrant virtual machine
     To use the Vagrant virtual machine, navigate to the full-stack-nanodegree-vm/tournament directory in the terminal, then use the command vagrant up (powers on the virtual machine) followed by vagrant ssh (logs into the virtual machine).  
 2. Launches and connects to tournament database:
-    vagrant@trusty32: psql tournament
+    vagrant@trusty32: psql tournament -f tournament.sql
 3. Run Python unit test module:
     vagrant@trusty32: python tournament_test.py
+4. Verify the test result will show "Success! All test pass!".
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,12 +6,16 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament
+
 CREATE TABLE players ( name TEXT,
                      id SERIAL primary key);
 
 CREATE TABLE matches ( match_id SERIAL primary key,
-	                   winner_id SERIAL REFERENCES players,
-	                   loser_id SERIAL REFERENCES players);
+                       winner_id INTEGER REFERENCES players,
+                       loser_id INTEGER REFERENCES players);
 
 -- Create view for player standings.
 -- Wins is got from the player table left joining the matches table 
@@ -20,10 +24,10 @@ CREATE TABLE matches ( match_id SERIAL primary key,
 -- where the player id is winning id or loser id;
 CREATE VIEW playerStandings AS 
     SELECT players.id, players.name, w.wins, tm.total_matches FROM players 
-        LEFT JOIN (SELECT players.id, count(winner_id) AS wins FROM players 
-        	           LEFT JOIN matches ON players.id = winner_id 
-        	           GROUP BY players.id) AS w ON players.id = w.id
-        LEFT JOIN (SELECT players.id, count(match_id) AS total_matches FROM players 
-        	           LEFT JOIN matches ON players.id = winner_id OR players.id = loser_id 
-        	           GROUP BY players.id) AS tm ON players.id = tm.id
+        LEFT JOIN (SELECT players.id, COUNT(winner_id) AS wins FROM players 
+                       LEFT JOIN matches ON players.id = winner_id 
+                       GROUP BY players.id) AS w ON players.id = w.id
+        LEFT JOIN (SELECT players.id, COUNT(match_id) AS total_matches FROM players 
+                       LEFT JOIN matches ON players.id = winner_id OR players.id = loser_id 
+                       GROUP BY players.id) AS tm ON players.id = tm.id
         ORDER BY w.wins DESC;
